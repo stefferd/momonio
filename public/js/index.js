8e6aebdb87be09b1b70aaf1481e537d4a9164752
stefferd <vandenberg.stef@gmail.com>
2018-01-15 21:31:52 +0100

    Updated the google analytics tag

--- public/js/index.js
+++ public/js/index.js
@@ -26,5 +26,5 @@ $( document ).ready(function() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'UA-64947338-3');
+    gtag('config', 'UA-64947338-4');
 });
